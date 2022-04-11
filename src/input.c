ffe1c05005ab24adde7509da9c4eb158ee52b7a7
juanRodriguez <juanmrodriguez1989@gmail.com>
2022-04-11 18:54:10 -0300

    ahi va mas completa

--- src/input.c
+++ src/input.c
@@ -34,3 +34,68 @@ int getInt(int * pNumeroIngresado,char * mensaje,char * mensajeError,
 
 	return retorno;
 }
+
+/* estas estan hechas por mi 08/04/2022 - hacerla mierda con el apunte de inputs
+
+int UTN_getInt(int* pResultado, char* mensaje, char* mensajeError, int minimo, int maximo, int reintentos)
+{
+	int aux;
+	int i;
+	int retorno;
+
+	retorno=1; // salio mal
+
+	if(pResultado!=NULL && mensaje!=NULL && mensajeError!=NULL && minimo<maximo && reintentos>=0)
+	{
+		for(i=0; i<reintentos; i++)
+		{
+			printf("%s", mensaje);
+			scanf("%d", &aux);
+			if(aux>=minimo && aux<=maximo)
+			{
+				retorno=0; // salio bien
+				*pResultado=aux;
+				break;
+			}
+			else
+			{
+				printf("%s", mensajeError);
+				retorno=1;
+			}
+		}
+	}
+	return retorno;
+}
+
+int UTN_getChar(char* pResultado, char* mensaje, char* mensajeError, int minimo, int maximo, int reintentos)
+{
+		char aux;
+		int i;
+		int retorno;
+
+		retorno=1; // salio mal
+
+		if(pResultado!=NULL && mensaje!=NULL && mensajeError!=NULL && minimo<maximo && reintentos>=0)
+		{
+			for(i=0; i<reintentos; i++)
+			{
+				printf("%s", mensaje);
+				scanf("%c", &aux);
+				if(aux>=minimo && aux<=maximo)
+				{
+					retorno=0; // salio bien
+					*pResultado=aux;
+					break;
+				}
+				else
+				{
+					printf("%s", mensajeError);
+					retorno=1;
+				}
+			}
+		}
+		return retorno;
+}
+
+ */
+
