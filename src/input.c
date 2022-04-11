e99be088a9908cfe3d6486a3c0c4b8ec262af53b
juanRodriguez <juanmrodriguez1989@gmail.com>
2022-04-11 18:44:32 -0300

    lo subimo

---
+++ src/input.c
@@ -0,0 +1,36 @@
+/*
+ * input.c
+ *
+ *  Created on: 7 abr 2022
+ *      Author: juanm
+ */
+
+#include "input.h"
+#include <stdio.h>
+
+int getInt(int * pNumeroIngresado,char * mensaje,char * mensajeError,
+		int maximo, int minimo, int maximoDeReintentos){
+
+	int auxNumeroIngresado;
+	int retorno;
+	retorno = -1;
+
+	if(pNumeroIngresado != NULL && maximo >= minimo && maximoDeReintentos>= 0){
+		do{
+			printf("%s", mensaje);
+			scanf("%d", &auxNumeroIngresado);
+			maximoDeReintentos--;
+
+			if(auxNumeroIngresado >= minimo && auxNumeroIngresado <= maximo){
+						*pNumeroIngresado = auxNumeroIngresado;
+						retorno = 0;
+						break;
+			}else{
+				printf("%s", mensajeError);
+			}
+
+		}while(maximoDeReintentos > 0);
+	}
+
+	return retorno;
+}
---
+++ src/input.h
@@ -0,0 +1,23 @@
+/*
+ * input.h
+ *
+ *  Created on: 7 abr 2022
+ *      Author: juanm
+ */
+
+#ifndef INPUT_H_
+#define INPUT_H_
+
+/// @brief Esta funcion hace...
+///
+/// @param pNumeroIngresado
+/// @param mensaje
+/// @param mensajeError
+/// @param maximo
+/// @param minimo
+/// @param maximoDeReintentos
+/// @return
+int getInt(int * pNumeroIngresado,char * mensaje,char * mensajeError,int maximo, int minimo, int maximoDeReintentos);
+
+
+#endif /* INPUT_H_ */
